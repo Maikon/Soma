005dba8fb5a699657f4f988f6a9087371ebf14d0
Makis Otman <makisotman@gmail.com>
2014-03-18 11:34:38 +0000

    added tests checking range for Hb and MCV methods

--- app/models/medical_tests/blood_test.rb
+++ app/models/medical_tests/blood_test.rb
@@ -2,4 +2,8 @@ class BloodTest < MedicalTest
   def has_healthy_hb?
     hb >= 12 && hb <= 16
   end
+
+  def has_healthy_mcv?
+    mcv >= 80 && mcv <= 100
+  end
 end
--- spec/models/blood_test_spec.rb
+++ spec/models/blood_test_spec.rb
@@ -3,8 +3,26 @@ require 'spec_helper'
 describe BloodTest do
   let(:test) { BloodTest.new }
 
-  it 'should know if Hb is out of range' do
-    test.hb = 300
-    expect(test).not_to have_healthy_hb
+  context 'Checking for ranges' do
+
+    it 'should know if Hb is within range' do
+      test.hb = 15
+      expect(test).to have_healthy_hb
+    end
+
+    it 'should know if Hb is out of range' do
+      test.hb = 300
+      expect(test).not_to have_healthy_hb
+    end
+
+    it 'should know if MCV is within range' do
+      test.mcv = 90
+      expect(test).to have_healthy_mcv
+    end
+
+    it 'should know if MCV is out of range' do
+      test.mcv = 70
+      expect(test).not_to have_healthy_mcv
+    end
   end
 end
