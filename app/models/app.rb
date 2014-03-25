b3c9afc9f90516591a740cbb506ed5b4ddda5e1d
Megan Folsom <mfolsom@gmail.com>
2014-03-25 17:42:11 +0000

    added User model and relationship with App

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,4 +1,5 @@
 class App < ActiveRecord::Base
+  has_many :users
   def self.register(name)
     app = App.new(name: name)
     app.key = (1..50).map { (("A".."Z").to_a + ("a".."z").to_a + (1..9).to_a).sample }.join
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140325162955) do
+ActiveRecord::Schema.define(version: 20140325173519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 20140325162955) do
     t.float    "creatinine"
     t.float    "esr"
     t.string   "crp"
+  end
+
+  create_table "users", force: true do |t|
+    t.integer  "app_id"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
